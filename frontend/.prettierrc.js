502358c2e2a4722728f3d4af5119290b80f18623
Viwe Teko <iamviwe.teko@gmail.com>
2025-12-12 19:27:40 +0000

    Empty-ish Prettier file for TS

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Prettier config will NOT include printWidth: 80
+    // because we are forcing ESLint's 'max-len' to handle it.
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'all',
+    jsxSingleQuote: true,
+    arrowParens: 'always',
+  };
