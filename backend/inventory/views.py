c4580534214ae5ec0dd642bd774da831635fbab5
Viwe Teko <iamviwe.teko@gmail.com>
2025-12-27 23:13:39 +0000

    Views includes hasattr

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,15 +1,18 @@
-from rest_framework import permissions, viewsets
+from rest_framework import exceptions, permissions, viewsets
 
 from .models import Organization, Unit
 from .serializers import OrganizationSerializer, UnitSerializer
 
 
 class OrganizationViewSet(viewsets.ModelViewSet):
+    """ Read-only viewset for Organizations."""
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
+    permission_classes = [permissions.IsAuthenticated]  # noqa: RUF012
     
 
 class UnitViewSet(viewsets.ModelViewSet):
+    """ CRUD viewset for Units. """
     # queryset = Unit.objects.all()
     serializer_class = UnitSerializer
     permission_classes = [permissions.IsAuthenticated]  # noqa: RUF012
@@ -17,15 +20,15 @@ class UnitViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Filters users to only see their own units"""
         user = self.request.users
-        if user.organization:
+        if hasattr(user, 'organization') and user.organization:
             return Unit.objects.filter(current_owner=user.organization)
         return Unit.objects.none()
 
     def perform_create(self, serializer):
         """Sets the current_owner to the user's organization"""
         user = self.request.user
-        if not user.organization:
-            raise permissions.exceptions.PermissionDenied(
+        if not hasattr(user, 'organization') or not user.organization:
+            raise exceptions.PermissionDenied(
                 "User must belong to an Organization to create units."
             )
         serializer.save(current_owner=user.organization)
