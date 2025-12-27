bcccbdff87c6f48c8c74b19c4651680685c86877
Viwe Teko <iamviwe.teko@gmail.com>
2025-12-27 23:22:08 +0000

    Serializer changed and added fields

--- backend/inventory/serializers.py
+++ backend/inventory/serializers.py
@@ -11,14 +11,20 @@ from .models import Organization, Unit
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
-        fields = ['id', 'name', 'type']  # noqa: RUF012
+        fields = [  # noqa: RUF012
+            'id', 'name', 'licence_number', 'address',
+            'contact_email', 'is_active', 'created_at'
+            ]
+        read_only_fields = ['id', 'created_at']  # noqa: RUF012
+
+
 
 class UnitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Unit
         fields = [  # noqa: RUF012
             'id',
-            'name',
+            'cultivar_name',
             'weight',
             'unit_type',
             'current_owner',
@@ -28,11 +34,14 @@ class UnitSerializer(serializers.ModelSerializer):
             'lab_test_status',
             'storage_location',
             'description',
+            'gps_coordinates',
+            'quality_grade',
+            'date_harvested'
             ]
         read_only_fields = ['id', 'created_at','current_owner']  # noqa: RUF012
 
-        def validate_weight(self, value: float) -> float:
-            """ This validates the weight"""
-            if value <= 0:
-                raise serializers.ValidationError("Weight must be positive.")
-            return value
+    def validate_weight(self, value: float) -> float:
+        """ This validates the weight"""
+        if value <= 0:
+            raise serializers.ValidationError("Weight must be positive.")
+        return value
